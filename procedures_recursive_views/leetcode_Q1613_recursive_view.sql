d9c34cac2264644ae275c1c194b9fad84fa2c965
Irene Zhou <57390524+irenejiazhou@users.noreply.github.com>
2023-09-30 11:03:50 -0400

    Create leetcode_Q1613_recursive_view.sql

---
+++ procedures_recursive_views/leetcode_Q1613_recursive_view.sql
@@ -0,0 +1,48 @@
+-- MEDIUM 1613: Find the Missing IDs
+/*
+Table: Customers
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |  PK
+| customer_name | varchar |
++---------------+---------+
+Each row of this table contains the name and the id customer.
+ 
+Write an SQL query to find the missing customer IDs. 
+The missing IDs are ones that are not in the Customers table but are in the range between 1 and the maximum customer_id present in the table.
+Notice that the maximum customer_id will not exceed 100.
+Return the result table ordered by ids in ascending order.
+
+The query result format is in the following example.
+Input: 
+Customers table:
++-------------+---------------+
+| customer_id | customer_name |
++-------------+---------------+
+| 1           | Alice         |
+| 4           | Bob           |
+| 5           | Charlie       |
++-------------+---------------+
+Output: 
++-----+
+| ids |
++-----+
+| 2   |
+| 3   |
++-----+
+Explanation: 
+The maximum customer_id present in the table is 5, so in the range [1,5], IDs 2 and 3 are missing from the table.
+*/
+
+WITH RECURSIVE seq AS (SELECT 1 AS num 
+											 
+                       UNION ALL 
+											 
+                       SELECT num + 1 
+											 FROM seq 
+											 WHERE num < 100) -- The maximum customer_id will not exceed 100.
+SELECT num AS ids
+FROM seq
+WHERE num NOT IN (SELECT customer_id FROM customers)
+  AND num <= (SELECT max(customer_id) FROM customers);
